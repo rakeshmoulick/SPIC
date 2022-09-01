141576d7026aba58e24f55bbac48c6a25b97db05
Rakesh Moulick <rakesh.moulick@gmail.com>
2022-09-01 10:05:33 +0530

    update: particle_data folder. particle data scripts are merged in scripts folder.

---
+++ scripts/dispersion_npz_beamdensity.py
@@ -0,0 +1,166 @@
+"""
+This file plots the dispersion graph from the npz file processed_results_E.npz
+Use %reset -f to clear the python workspace 
+"""
+# TO RUN: python3 dispersion_npz.py
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mp
+from scipy.constants import value as constants
+import os.path
+from os.path import join as pjoin
+
+file_name = 'processed_results_all.npz'
+path = './'
+
+# Constants
+eps0 = constants('electric constant')
+kb = constants('Boltzmann constant')
+me = constants('electron mass')
+AMU = constants('atomic mass constant')
+e = constants('elementary charge')
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NUM_TS = 50000
+write_interval = 5
+DT_coeff = 0.01
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+vd = 20;
+n0 = 1E10;
+Tec = 1*e;
+Teh = 100*e;
+Teb = 1*e;
+Ti = 0.026*e;
+mi = 40*AMU;
+#-------------------------------------------------------
+alp = 1.0;
+beta = 0.04;
+#------------------------------------------------------
+# SIM Vars
+NC = 1024;
+Time = 0;
+#-----------------------------------------------------
+ni0 = (1+alp+beta)*n0;
+nec0 = n0;
+neh0 = alp*n0;
+neb0 = beta*n0;
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+DATA_TS = int(NUM_TS/write_interval) + 1
+
+LDH = np.sqrt(eps0*Teh/(neh0*e**2)) # Hot electron Debye Length
+LDC = np.sqrt(eps0*Tec/(nec0*e**2)) # Cold electron Debye Length
+LDB = np.sqrt(eps0*Tec/(nec0*e**2)) # Cold electron Debye Length
+LD = (LDC*LDC*LDH)/(LDH*LDB + LDC*LDB + LDC*LDH)
+
+wpi = np.sqrt(e**2*ni0/(eps0*mi))
+wp = np.sqrt(e**2*ni0/(eps0*me)) # Total Plasma Frequency
+
+wpec = np.sqrt((nec0*e**2)/(eps0*me))
+wpeh = np.sqrt(e**2*neh0/(eps0*me))
+wpeb = np.sqrt(e**2*neb0/(eps0*me))
+
+ud = vd*(LDC*wpec);
+DT = DT_coeff*(1.0/wp)
+#++++++++++++++++++++ UNPACK ++++++++++++++++++++++++++++++++++++++++++++++++++
+if os.path.exists(pjoin(path,file_name)):
+    data = np.load(pjoin(path,file_name))
+    x = data['x']
+    ndeb = data['ndeb']    
+    
+else:
+    print('No data')
+    exit()
+
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+x = x.reshape(DATA_TS,(NC+1))
+x = x[0,:] # SPATIAL GRID: Select only a column, all the columns have the same value
+dx = x[1]-x[0] # Normalized StepSize
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+"""
+In each row of EF, there are NC+1 = 1025 columns. Data is put in the first row first
+and then it goes into the second row. Therefore, all the values of the EF for the
+first time step is written in first row in 1025 columns. The data of next time step
+is in the second row and so on. Similar is the case for writing and reshaping x.
+w_pe*t = NUM_TS*DT
+"""
+ndeb = ndeb.reshape(DATA_TS,(NC+1))
+mat = beta*np.ones(ndeb.shape)
+ndeb = ndeb - mat
+print("The shape of ndeb is: ", ndeb.shape)
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+wpet_1 = 0 #1000
+wpet_2 = 500
+y1 = wpet_1/(DT_coeff*write_interval)
+y2 = wpet_2/(DT_coeff*write_interval)
+
+ndb = ndeb[int(y1):int(y2),:]
+print("The shape of ndb is: ",ndb.shape)
+
+#+++++++++++++++++++++++++ FFT of E-field data ++++++++++++++++++++++++++++++++
+Fb = np.fft.fftn(ndb, norm = 'ortho')
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Define Omega and K
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NUM_TS1 = wpet_1/DT_coeff
+NUM_TS2 = wpet_2/DT_coeff
+actual_sim_time = (NUM_TS2 - NUM_TS1)*(DT) # Previously it was (NUM_TS*DT)
+# -----------------------------------------------------------------------------
+omega = 2*np.pi*np.arange(NUM_TS)/(actual_sim_time) #(DT*NUM_TS) #(actual_sim_time) # Unnormalized Omega
+k     = 2*np.pi*np.arange(NC+1)/(NC*dx*LDC)       # Normalized k
+print('Length of k: ',len(k))
+print('Max of k: ',np.max(k))
+
+Omega, K = np.meshgrid(omega, k, indexing='ij')
+print('Shape of Omega: ',Omega.shape)
+#------------------------------------------------------------------------------
+halflen = np.array(Fb.shape, dtype=int)//2
+Omega = Omega[:halflen[0],:halflen[1]]
+K = K[:halflen[0],:halflen[1]]
+Fb = Fb[:halflen[0],:halflen[1]]
+Omega /=wp # Normalized Omega 
+K = K*LDC  # Normalized K
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+Zb = np.log(np.abs(Fb))
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+figsize = np.array([80,80/1.618]) #Figure size in mm (FOR SINGLE FIGURE)
+dpi = 1200                        #Print resolution
+ppi = np.sqrt(1920**2+1200**2)/24 #Screen resolution
+
+mp.rc('text', usetex=False)
+mp.rc('font', family='sans-serif', size=10, serif='Computer Modern Roman')
+mp.rc('axes', titlesize=10)
+mp.rc('axes', labelsize=10)
+mp.rc('xtick', labelsize=10)
+mp.rc('ytick', labelsize=10)
+mp.rc('legend', fontsize=10)
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+fig,ax = plt.subplots(figsize=figsize/25.4,constrained_layout=True,dpi=ppi)
+
+plt.pcolor(K, Omega, Zb, cmap='rainbow',shading='auto',vmin=-12,vmax=-4)
+#cbar = plt.colorbar()
+#cbar.set_label('$\Delta n_{c}$')
+
+ax.set_xlim([0, 1.0])
+ax.set_ylim([0, 3.0])
+ax.set_xlabel('$k \lambda_{Dc}$')
+ax.set_ylabel('$\omega/\omega_{pe}$')
+plt.savefig(pjoin(path,'dispersion_beamdensity_FFT1_%d_%d.png'%(wpet_1,wpet_2)),dpi=dpi)
+
+"""
+wpet = NUM_TS*DT_coeff # It is 500 in this case
+xx = np.linspace(0, NC, (NC+1))
+yy = np.linspace(0, wpet, DATA_TS)
+
+plt.pcolor(xx, yy, ndeb, cmap='viridis', shading='auto')
+cbar = plt.colorbar()
+cbar.set_label('$\Delta n_{b}$')
+
+ax.set_xlabel('$x$')
+ax.set_ylabel('$\omega_{pe}t$')
+"""
+
+plt.show()
---
+++ scripts/particle_fixedtime.py
@@ -0,0 +1,76 @@
+# This file plots the particle data at fixed times. This is the file to generate plots for the paper.
+# Only citing the path to the data file is enough to plot,
+# No specific input corresponding to parameteric variation is required
+import numpy as np
+import matplotlib.pyplot as plt
+import os.path
+from os.path import join as pjoin
+
+path = './data/'
+path_fig = './'
+# ------------- Comments -------------------------------------------------------
+# Path to data file for vd=20 is
+# ../data/particle_data/part_vd_20/data
+# Path to data file for vd=80 is
+# ../data/particle_data/part_vd_80/data
+# Figures to be saved in ../data/particle_data/part_vd_20/figs for vd=20 &
+# ../data/particle_data/part_vd_80/figs for vd=80
+#-------------------------------------------------------------------------------
+# Define the time at which data is required
+DT_coeff = 0.01
+write_interval = 200
+# File index value is k(say), change this index to get the plot at required time.
+# Calculate the wpet for a k-value by using DT_coeff beforehand.
+k = 150000
+
+wpet = k*DT_coeff # Value of wpet
+
+file_name_ec = 'ec%d.txt'%(int(k))
+file_name_eh = 'eh%d.txt'%(int(k))
+file_name_eb = 'eb%d.txt'%(int(k))
+
+# Load Cold electron data
+if os.path.exists(pjoin(path,file_name_ec)):
+    xec,vec = np.loadtxt(pjoin(path,file_name_ec),unpack=True)
+else:
+    print('No data')
+    exit()
+
+# Load Hot electron data
+if os.path.exists(pjoin(path,file_name_eh)):
+    xeh,veh = np.loadtxt(pjoin(path,file_name_eh),unpack=True)
+else:
+    print('No data')
+    exit()
+
+# Load Beam electron data
+if os.path.exists(pjoin(path,file_name_eb)):
+    xeb,veb = np.loadtxt(pjoin(path,file_name_eb),unpack=True)
+else:
+    print('No data')
+    exit()
+
+# Plot the figure
+
+# In scatter plot s specifies the area
+plt.figure(1)
+plt.scatter(xec,vec,color='b',marker='.',s=0.1,alpha=0.5)
+plt.xlabel('$x_{ec}$')
+plt.ylabel('$\u03C5_{ec}$')
+plt.title("Cold Electron Phase Space at $\omega_{pe}t = %d$" %(wpet))
+plt.savefig(pjoin(path_fig,'ec_%d'%(wpet)),dpi=600)
+
+plt.figure(2)
+plt.scatter(xeh,veh,color='r',marker='.',s=0.1,alpha=0.5)
+plt.xlabel('$x_{eh}$')
+plt.ylabel('$\u03C5_{eh}$')
+plt.title("Hot Electron Phase Space at $\omega_{pe}t = %d$" %(wpet))
+plt.savefig(pjoin(path_fig,'eh_%d'%(wpet)),dpi=600)
+
+plt.figure(3)
+plt.scatter(xeb,veb,color='g',marker='.',s=0.1,alpha=0.5)
+plt.xlabel('$x_{eb}$')
+plt.ylabel('$\u03C5_{eb}$')
+plt.title("Beam Electron Phase Space at $\omega_{pe}t = %d$" %(wpet))
+plt.savefig(pjoin(path_fig,'eb_%d'%(wpet)),dpi=600)
+plt.show()
